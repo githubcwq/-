d14aa2baa8208d0896cd01be6004531d1975ea1f
曹文卿 <2632748308qq.com>
2019-04-17 22:28:28 +0800

    1

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello C# Programming 世界!");    //输出语句
+            Console.ReadKey();     ///等待键盘输入
+        }
+    }
+}
